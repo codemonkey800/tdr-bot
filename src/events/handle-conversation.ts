5dca7e7986803ea3db656830813ded4394d7562c
Jeremy Asuncion <jeremyasuncion808@gmail.com>
2023-05-28 01:38:36 -0700

    handle image generation

--- src/events/handle-conversation.ts
+++ src/events/handle-conversation.ts
@@ -1,12 +1,15 @@
-import { Message } from 'discord.js'
+import { EmbedBuilder, Message } from 'discord.js'
 import { injectable } from 'inversify'
+import { OpenAI, PromptTemplate } from 'langchain'
 import {
   AgentExecutor,
   initializeAgentExecutorWithOptions,
 } from 'langchain/agents'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
+import { StructuredOutputParser } from 'langchain/output_parsers'
 import { SerpAPI } from 'langchain/tools'
 import { Calculator } from 'langchain/tools/calculator'
+import { Configuration, OpenAIApi } from 'openai'
 import { logger } from 'src/core/logger'
 import { getClientID } from 'src/core/utils'
 
@@ -45,6 +48,93 @@ export class HandleConversationOnMessageEvent
     logger.log('Executor created!')
   }
 
+  private async getGenerateImageDescription(
+    message: string,
+  ): Promise<string | null> {
+    const parser = StructuredOutputParser.fromNamesAndDescriptions({
+      imageDescription: 'description of image to generate',
+      isGenerating: 'whether to generate image or not',
+    })
+    const instructions = parser.getFormatInstructions()
+
+    const prompt = new PromptTemplate({
+      template:
+        'Return "yes" or "no" if the following message is asking to generate an image.\nStore it in JSON with the properties "imageDescription" for the image description and "isGenerating" that is "yes" or "no" if the image is generating or not.\nThe message is defined below:\n{message}',
+      inputVariables: ['message'],
+      partialVariables: {
+        format_instructions: instructions,
+      },
+    })
+
+    const model = new OpenAI({ temperature: 0 })
+    const input = await prompt.format({ message })
+    const response = await model.call(input)
+
+    const { imageDescription, isGenerating } = await parser.parse(response)
+    if (isGenerating === 'yes') {
+      return imageDescription
+    }
+
+    return null
+  }
+
+  private async sendImageMessage(
+    message: Message<boolean>,
+    description: string,
+  ) {
+    logger.log(`Generating image for "${description}"...`)
+
+    const config = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY as string,
+    })
+    const api = new OpenAIApi(config)
+
+    let imageUrl: string | undefined
+
+    try {
+      const { data } = await api.createImage({
+        prompt: description,
+        n: 1,
+        size: '512x512',
+      })
+
+      imageUrl = data.data[0]?.url
+    } catch (err) {
+      logger.error('Unable to generate image', err)
+    }
+
+    if (!imageUrl) {
+      await message.channel.send(
+        `Unfortunately, I could not generate an image for "${description}"`,
+      )
+      return
+    }
+
+    await message.channel.send({
+      embeds: [new EmbedBuilder().setTitle(description).setImage(imageUrl)],
+    })
+  }
+
+  private async sendChatMessage(
+    message: Message<boolean>,
+    formattedMessage: string,
+    author: string,
+  ) {
+    if (!this.executor) {
+      return
+    }
+
+    logger.log(`Calling executor with message: "${formattedMessage}"`)
+
+    const result = await this.executor.call({
+      input: `${author} said "${formattedMessage}"`,
+    })
+    const response = result.output as string
+
+    logger.log(`Responding with message: "${response}"`)
+    await message.channel.send(response)
+  }
+
   async handleEvent(message: Message<boolean>) {
     const clientId = getClientID()
 
@@ -61,7 +151,10 @@ export class HandleConversationOnMessageEvent
 
     await this.maybeInitExecutor()
     if (this.executor) {
-      logger.log(`Calling executor with message: "${message.content}"`)
+      const formattedMessage = message.mentions.users.reduce(
+        (msg, user) => msg.replaceAll(`<@${user.id}>`, user.username),
+        message.content,
+      )
 
       await message.channel.sendTyping()
       const typingInterval = setInterval(
@@ -69,14 +162,23 @@ export class HandleConversationOnMessageEvent
         11 * 1000,
       )
 
-      const result = await this.executor.call({
-        input: `${message.author.username} said "${message.content}"`,
-      })
-      const response = result.output as string
-      clearInterval(typingInterval)
+      const imageDescription = await this.getGenerateImageDescription(
+        formattedMessage,
+      )
 
-      logger.log(`Responding with message: "${response}"`)
-      await message.channel.send(response)
+      try {
+        if (imageDescription) {
+          await this.sendImageMessage(message, imageDescription)
+        } else {
+          await this.sendChatMessage(
+            message,
+            formattedMessage,
+            message.author.username,
+          )
+        }
+      } finally {
+        clearInterval(typingInterval)
+      }
     }
   }
 }
